27466cf905d44efdd70b01f679baad75fe89608b
Abhinav <abhinav.ranger@gmail.com>
2021-10-09 19:30:42 +0530

    Added counter

---
+++ folder_1/counter in js for website.js
@@ -0,0 +1,7 @@
+var i = 0;
+function myFunction(){
+    ++i;
+    document.getElementById('main_title').innerHTML = i;
+    kek = i + ' + 1';
+    document.getElementById('click').innerHTML = kek;
+}
