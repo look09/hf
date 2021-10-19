427434da68acba27178c7b43030fcf84c1b43270
Abhi <43322431+broken-cupnoodle@users.noreply.github.com>
2021-10-19 21:05:22 +0530

    json sorter

--- folder_1/main.py
+++ folder_1/main.py
@@ -1,11 +1,30 @@
-from bs4 import BeautifulSoup as BS
-# requests module allows you to send HTTP requests and returns a Response Object with all the response​.
-import requests
-def get_price(url):
-    data=requests.get(url) # Accessing all required data from url site and store in data.
-    soup=BS(data.text,"html.parser") # It takes text of page as argument and then parse it with html.parser
-    ans=soup.find("div",class_="BNeawe iBp4i AP7Wnd").text # here the class name is passed as argument to find out that particular info in html text
-    return ans
-url="https://www.google.com/search?q=bitcoin+price"
-ans=get_price(url)
-print("1 Bitcoin = ",ans)
+import requests, json, time
+
+
+mydict = {}
+nekos = [
+        'baka', 'bite', 'blush', 'bored', 'cry', 'cuddle',
+        'dance', 'facepalm', 'feed', 'happy', 'hug', 'kiss',
+        'laugh', 'pat', 'shrug', 'slap', 'sleep', 'smile',
+        'smug', 'stare', 'think', 'thumbsup', 'tickle', 'wave',
+        'wink'
+    ]
+
+nekos.sort(key=str.lower)
+
+for i in nekos:
+	k = []
+	for n in range(50):
+		res = requests.get(f'https://nekos.best/api/v1/{i}').json()
+		print(n,i,":", res['url'])
+		if res['url'] in k:
+			pass
+		else:
+			k.append(res['url'])
+		time.sleep(5)
+	dic = {i:k}
+	mydict.update(dic)
+f = open('emotes-url.json','a+')
+json.dump(mydict, f, indent=4)
+f.close()
+print("Done")
